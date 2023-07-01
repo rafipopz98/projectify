3824b5904203c2f059f5d5d34b49e2687a59232a
rafipopz98 <itsrealrafi@gmail.com>
2023-07-01 12:37:03 +0530

    global

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import GlobalStyles from "./styles/GlobalStyles";
+// import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
